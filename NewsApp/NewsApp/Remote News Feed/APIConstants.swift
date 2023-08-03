64e4a16060518ac467d4f8768c45cbedf1f1911c
Medhad <medhad@gmail.com>
2023-08-02 22:15:20 -0400

    Added a fake news url for test

--- NewsApp/NewsApp/Remote News Feed/APIConstants.swift
+++ NewsApp/NewsApp/Remote News Feed/APIConstants.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 struct APIConstants {
- static let newsURL = URL(string: "https://www.cbc.ca/aggregate_api/v1/items?lineupSlug=news")!
+ //static let newsURL = URL(string: "https://www.cbc.ca/aggregate_api/v1/items?lineupSlug=news")!
+ static  let newsURL = URL(string: "https://raw.githubusercontent.com/leovinci7/dummy-news-json/main/db2.json")!
 }
